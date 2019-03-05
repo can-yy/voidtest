ccde7a1542deaf0dfb18ed8e2d8a7e3151cbaa8d
can-yy <1819697566@qq.com>
2019-03-05 11:59:08 +0800

    提交到远程仓库

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!。。。。我是李四，我要改代码" );
+        System.out.println( "Hello World!。。。。" );
     }
 }
