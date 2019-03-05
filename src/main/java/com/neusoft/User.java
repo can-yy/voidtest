464ce716de63f3a56c65f434a1d6a11dea2e978f
can-yy <1819697566@qq.com>
2019-03-05 13:21:54 +0800

    提交到远程仓库

--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -2,7 +2,7 @@ package com.neusoft;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("测试git   和 github  管理，，，呐呐呐呐你你你你");
+        System.out.println("测试git   和 github  管理，，，我是张三");
 
 
     }
